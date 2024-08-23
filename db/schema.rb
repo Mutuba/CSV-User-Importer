e714ffcba71d384c136d8d23d6efe833b068c117
Mutuba <danielmutubait@gmail.com>
2024-08-23 15:37:29 +0300

    chore: Add not null constraint for name and password

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,29 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_823_113_954) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_23_113954) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'password'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-
-    # Prevent three repeating characters in a row
-    t.check_constraint \
-      "NOT password::text ~ '(.)\\1\\1'::text",
-      name: 'password_no_repeating_characters_check'
-
-    # Ensure the password length is between 10 and 16 characters
-    t.check_constraint \
-      'char_length(password::text) >= 10 AND char_length(password::text) <= 16',
-      name: 'password_length_check'
-
-    # Ensure the password contains at least one lowercase letter, one uppercase letter, and one digit
-    t.check_constraint \
-      "password::text ~ '[a-z]'::text AND password::text ~ '[A-Z]'::text AND password::text ~ '[0-9]'::text",
-      name: 'password_complexity_check'
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.check_constraint "NOT password::text ~ '(.)\\1\\1'::text", name: "password_no_repeating_characters_check"
+    t.check_constraint "char_length(password::text) >= 10 AND char_length(password::text) <= 16", name: "password_length_check"
+    t.check_constraint "password::text ~ '[a-z]'::text AND password::text ~ '[A-Z]'::text AND password::text ~ '[0-9]'::text", name: "password_complexity_check"
   end
+
 end
