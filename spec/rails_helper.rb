1cd016acfbfc27c76c1ecdc33e656e136ab55077
Mutuba <danielmutubait@gmail.com>
2024-08-23 16:49:16 +0300

    chore: Clean up tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,6 @@
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 require "database_cleaner"
