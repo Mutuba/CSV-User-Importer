56f12b219c3e033036a0d0b9df45eb2b9bf09010
Mutuba <danielmutubait@gmail.com>
2024-08-25 17:28:46 +0300

    chore: Attempt to serve public files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,8 @@ Rails.application.configure do
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
-
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
